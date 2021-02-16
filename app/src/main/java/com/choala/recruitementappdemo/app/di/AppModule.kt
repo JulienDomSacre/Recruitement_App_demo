39472905eb8ad1be72e1a8a59bf155bba26037e0
Goldensnake <juliendom.sacre@gmail.com>
2021-02-16 23:59:22 +0100

    setup the DI

---
+++ app/src/main/java/com/choala/recruitementappdemo/app/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.choala.recruitementappdemo.app.di
+
+import com.choala.recruitementappdemo.data.di.localModule
+import com.choala.recruitementappdemo.data.di.remoteModule
+
+
+val appModule = listOf(
+    remoteModule,
+    localModule
+)
