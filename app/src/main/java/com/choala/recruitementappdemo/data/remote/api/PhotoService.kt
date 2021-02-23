9611005182a1fe06bd7f7f89028e866e44359131
Goldensnake <juliendom.sacre@gmail.com>
2021-02-23 09:10:30 +0100

    Update data for photo remote bug

--- app/src/main/java/com/choala/recruitementappdemo/data/remote/api/PhotoService.kt
+++ app/src/main/java/com/choala/recruitementappdemo/data/remote/api/PhotoService.kt
@@ -2,9 +2,10 @@ package com.choala.recruitementappdemo.data.remote.api
 
 import com.choala.recruitementappdemo.data.remote.response.PhotoResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PhotoService {
     @GET("users/1/photos")
-    suspend fun getPhotoList(@Query("albumID") id: Int): List<PhotoResponse>
+    suspend fun getPhotoList(@Query("albumId") id: Int): List<PhotoResponse>
 }
--- app/src/main/java/com/choala/recruitementappdemo/data/remote/response/PhotoResponse.kt
+++ app/src/main/java/com/choala/recruitementappdemo/data/remote/response/PhotoResponse.kt
@@ -1,9 +1,13 @@
 package com.choala.recruitementappdemo.data.remote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class PhotoResponse(
+    @SerializedName("albumId")
     val albumId: Int,
     val id: Int,
     val title: String,
     val url: String,
+    @SerializedName("thumbnailUrl")
     val thumbnailUrl: String
 )
